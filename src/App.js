2e53a4089ec9bd0090c02d4cc8110b855edfefd9
srepon <a1702993@myy.haaga-helia.fi>
2020-12-13 19:17:22 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Carlist from'./components/Carlist';
+
+function App() {
+  return (
+    <div className="App">
+      <Carlist />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Contact.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Contact = () => {
+    return (
+        <div>
+            <h1>Tämä on yhteystietosivu</h1>        
+        </div>
+    )
+}
+
+export default Contact;
